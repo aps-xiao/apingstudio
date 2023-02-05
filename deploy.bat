dc56d6bc0de9b4f17ffef09bde53828a9d0f465d
aps-xiao <406960301@qq.com>
2023-02-05 14:57:14 +0800

    push

---
+++ deploy.bat
@@ -0,0 +1,16 @@
+git init 
+git add .
+git commit -m "push"
+git remote add origin git@github.com:aps-xiao/apingstudio.git
+git branch -M main
+git push -u origin main
+yarn run docs:build
+cd .\src\.vuepress\dist\
+echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:aps-xiao/apingstudio.git main:gh-pages
+
+cd -
