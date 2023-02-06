e0f27cfcc766ca0c0019b381d6ab5d7960430db8
aps-xiao <406960301@qq.com>
2023-02-06 22:56:39 +0800

    push

--- deploy.bat
+++ deploy.bat
@@ -7,16 +7,13 @@ git remote add origin git@github.com:aps-xiao/apingstudio.git
 git branch -M main
 git push -u origin main
 yarn docs:build
-
 echo "push distance"
 cd .\src\.vuepress\dist\
 echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 git remote add origin git@github.com:aps-xiao/apingstudio.git
 git branch -M gh-pages
 git push -f git@github.com:aps-xiao/apingstudio.git main:gh-pages
-
 cd ..\..\..\
